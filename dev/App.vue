96dafd5ea25f8b83cb3d7b1b023118b93800f0b3
cooperateDev <pavelasanov1206@gmail.com>
2019-05-16 14:21:57 -0400

    fix: fixes name for browser field

---
+++ dev/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Vue2Editor</h1>
+    <vue-editor v-model="content" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: ""
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
